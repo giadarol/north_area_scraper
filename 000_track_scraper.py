0adc9840f1433ec355b1a33e22e6933c4ab806e6
giadarol <giovanni.iadarola@cern.ch>
2026-02-06 14:24:50 +0100

    Preparing to track

--- 000_track_scraper.py
+++ 000_track_scraper.py
@@ -5,5 +5,18 @@ import xtrack as xt
 
 fieldmap_file = "cm70.fluk.gz"
 
-field_interpolator = intp.load_field_map(fieldmap_file)
+field_interpolator_2d = intp.load_field_map(fieldmap_file)
+
+def field_3d(x, y, s):
+    """Wrapper to call the 3D field interpolator."""
+    bx, by = field_interpolator_2d(x, y)
+    bz = np.zeros_like(bx)  # Assuming Bz is zero
+    return bx, by, bs
+
+length_scraper = 3.0  # m
+num_slices = 20
+
+length_slice = length_scraper / num_slices
+
+
 
